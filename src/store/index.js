4fa78e813bd7ae0d96183839d114d70e179dfb41
Tenohira <tenohira.taro@gmail.com>
2021-01-31 17:34:36 +0900

    ホーム画面見た目完成

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    name: '',
+    character: '',
+    level: '',
+    infos: [],
   },
   mutations: {
+    changeName(state, v) {
+      state.name = v
+    }
   },
   actions: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,46 @@
 <template>
-  <p>home</p>
+  <v-container class="mx-auto my-auto">
+    <v-form ref="textField">
+      <v-text-field
+        label="あなたの名前"
+        outlined
+        append-icon="mdi-account-check"
+        :rules="[rule]"
+        color="green"
+        @click:append="start"
+        @keypress.enter.prevent
+        @keypress.enter="start"
+      ></v-text-field>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
 export default {
+  computed: {
+    name: {
+      get() {
+        return this.$store.state.name
+      },
+      set(v) {
+        this.$store.commit('changeName', v)
+      }
+    }
+  },
+  
+  methods: {
+    start() {
+      if(this.$refs.textField.validate()) {
+        console.log('start')
+      }
+    }
+  },
 
+  data() {
+    return{
+      rule:(v) => !!v || 'ちゃんと入力しろ！'
+    }
+  }
 }
 </script>
 
