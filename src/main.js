1dd846f2235cf888e49476fdfedd8323bd79a7af
Tenohira <tenohira.taro@gmail.com>
2021-01-31 17:23:25 +0900

    最小化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyBdvU9aJKzIxTTdizfOdHk46QjwSR0DzGg",
+  authDomain: "palami-apps.firebaseapp.com",
+  databaseURL: "https://palami-apps.firebaseio.com",
+  projectId: "palami-apps",
+  storageBucket: "palami-apps.appspot.com",
+  messagingSenderId: "1000349405707",
+  appId: "1:1000349405707:web:5318ea7f45a0056f93ed0e",
+  measurementId: "G-1JY6C6RGSX"
+};
+
+firebase.initializeApp(config);
+firebase.analytics();
 
 Vue.config.productionTip = false
 
